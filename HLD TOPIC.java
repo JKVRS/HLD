1237966c1902a19f5e2bfa18b0a2c42faaae8e84
JKVRS <128927884+JKVRS@users.noreply.github.com>
2024-03-10 16:11:02 +0530

    Add files via upload
    HLD Topics

---
+++ HLD TOPIC.java
@@ -0,0 +1,51 @@
+HLD TOPIC:
+1. Why HLD
+2. Scalling 
+      Vertical Scalling 
+      Horizantal Scalling
+3. LoadBalancer
+    StateFul
+    Stateless
+4. Consistenet Hashing
+5. Cache
+    CDN
+    Browser Cache
+    Global Cache : Redis
+    App Server Cache
+    DB Cache
+6. Cache Enviction
+7. Cache TTL(Time to Live)
+8. Cache Write 
+    Write Through Cache
+    Write Back Cache
+9. CAP Theoram
+10. PACELC (Partition- Available/Consistenet Else-Latency/Consistenet)
+11. DB- SQL vs NOSQL
+12. SQL- ACID properties
+13. NOSQL
+      Key-Value: Redis
+      Document DB: MogoDB , Dynamo DB
+      Column-Value DB: HBASE,CASANDRA
+14. Internal of NOSQL : How data store and update in NO-SQL DB
+15. Replaction 
+    Master-Slave
+16. Sharding : Choose sharding Key
+17. HDFS
+18. Nearest Neighbour
+19. Quad Tree
+20. Microservices
+      Distributed Transaction 
+      SAGA Design Pattern
+21. Zookeeper
+22. Kafka
+23. Case Studies:
+     TypeHeas Design
+     Messagener Design
+     Elastic Search Design 
+     Uber Design
+     Generate Unique ID 
+     Rate Limiter
+     HostStar Design
+     IRCTC Design
+24. Radis Discussion
+
